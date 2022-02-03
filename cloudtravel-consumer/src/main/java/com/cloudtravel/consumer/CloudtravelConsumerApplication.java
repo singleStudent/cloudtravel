96f3ea84656089b6f965a7b5ffc51664f653fdfb
learner <1260586157@qq.com>
2022-02-03 15:37:36 +0800

    common包中redis工具类相关环境变量引入

--- cloudtravel-consumer/src/main/java/com/cloudtravel/consumer/CloudtravelConsumerApplication.java
+++ cloudtravel-consumer/src/main/java/com/cloudtravel/consumer/CloudtravelConsumerApplication.java
@@ -3,6 +3,7 @@ package com.cloudtravel.consumer;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDubbo
 @EnableScheduling
 @ImportResource(locations = {"classpath:spring/*.xml"})
+@ComponentScan({"com.cloudtravel.consumer.*.*" , "com.cloudtravel.common.redis"})
 public class CloudtravelConsumerApplication {
 
 	public static void main(String[] args) {
