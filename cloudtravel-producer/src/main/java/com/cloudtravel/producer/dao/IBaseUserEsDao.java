94e034958fcab4b3b6ce01a8a1e419d2adff3465
learner <1260586157@qq.com>
2023-05-10 20:40:11 +0800

    es

--- cloudtravel-producer/src/main/java/com/cloudtravel/producer/dao/IBaseUserEsDao.java
+++ cloudtravel-producer/src/main/java/com/cloudtravel/producer/dao/IBaseUserEsDao.java
@@ -1,8 +1,6 @@
 package com.cloudtravel.producer.dao;
 
 import com.cloudtravel.producer.model.BaseUserModel;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
